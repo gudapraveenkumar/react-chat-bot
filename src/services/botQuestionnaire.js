9f491b585f0a7958869cde53798bfc11064ee5e3
gudapraveenkumar <praveenguda.kumar@gmail.com>
2019-08-30 20:56:36 +0200

    Project architecture and add material ui

---
+++ src/services/botQuestionnaire.js
@@ -0,0 +1,12 @@
+export const botQuestions = [
+   {
+      sender: 'bot',
+      questionId: 'name',
+      questionText: 'What is your name?'
+   },
+   {
+      sender: 'bot',
+      questionId: 'age',
+      questionText: 'What is your age?'
+   }
+];
